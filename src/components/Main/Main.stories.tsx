83fb81e81d0d8293dd2df0a0af961e8b798302e8
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-12-05 20:51:31 -0300

    chore: setup storybook controls

--- src/components/Main/Main.stories.tsx
+++ src/components/Main/Main.stories.tsx
@@ -1,29 +1,13 @@
-import { withKnobs, text } from '@storybook/addon-knobs';
 import { ComponentMeta } from '@storybook/react';
 import { Main } from './index';
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs],
+  args: {
+    title: 'default title',
+    description: 'default description',
+  },
 } as ComponentMeta<typeof Main>;
 
-export const Basic = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS e Styled Components',
-    )}
-  />
-);
-
-export const Secondary = () => (
-  <Main
-    title={text('Title', 'React Avançado1234')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS e Styled Components',
-    )}
-  />
-);
+export const Basic: typeof Main = args => <Main {...args} />;
