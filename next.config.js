edd0d5ca0f89b86e4b2c292e39549be4be03fe9e
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-12-05 22:30:24 -0300

    chore: setup PWA

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = withPWA({
   reactStrictMode: true,
-}
+  pwa: {
+    dest: 'public',
+    disable: !isProduction,
+  },
+});
