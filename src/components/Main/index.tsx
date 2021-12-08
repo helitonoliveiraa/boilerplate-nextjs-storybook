93389aafb3b522caf1043155a9e54a4360777203
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-12-08 19:35:39 -0300

    chore: setup script build storybook to get and serving static files

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -7,7 +7,7 @@ export function Main({
   return (
     <S.Container>
       <img
-        src="/img/logo.svg"
+        src="img/logo.svg"
         alt="Imagem de um átomo e React Avançado escrito ao lado."
       />
       <h1>{title}</h1>
@@ -15,7 +15,7 @@ export function Main({
       <h2>{description}</h2>
 
       <img
-        src="/img/hero-illustration.svg"
+        src="img/hero-illustration.svg"
         alt="Um desenvolvedor de frente para uma tela com código."
       />
     </S.Container>
