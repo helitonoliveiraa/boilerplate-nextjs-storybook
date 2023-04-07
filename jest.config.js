0c72538d85ae6d92c42bcb940f38787563cb8594
Héliton Oliveira <heliton.oliveira88@gmail.com>
2023-04-07 09:53:24 -0300

    chore: setup test

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+};
